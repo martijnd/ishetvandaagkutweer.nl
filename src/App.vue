9f2af7dc7d53c128c1cb561aa17e3bdf6e9cfd59
Martijn Dorsman <martijn.dorsman@gmail.com>
2021-02-21 16:40:43 +0100

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+<div class="wrapper">
+  <HelloWorld />
+</div>
 </template>
 
 <script lang="ts">
@@ -20,8 +21,5 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
