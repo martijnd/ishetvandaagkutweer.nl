e054b878da54840becad3c6d210c64ced2642cdc
Martijn Dorsman <martijn.dorsman@gmail.com>
2021-02-21 17:57:26 +0100

    Basic functionality

--- pages/api/weather.tsx
+++ pages/api/weather.tsx
@@ -2,10 +2,10 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(req.query.city as string)}&appid=${process.env.WEATHER_API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.long}&appid=${process.env.WEATHER_API_KEY}`;
     const data = await fetch(url);
     const result = await data.json();
-    
+
     res.status(200).json({ result })
   }
   
