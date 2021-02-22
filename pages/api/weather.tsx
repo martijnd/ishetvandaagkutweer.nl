5446233dfd7ffe6d4ffb82d2f547b25c7dea3d86
Martijn Dorsman <martijn.dorsman@gmail.com>
2021-02-22 13:45:39 +0100

    Format

--- pages/api/weather.tsx
+++ pages/api/weather.tsx
@@ -1,14 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import fetch from 'node-fetch';
+import { NextApiRequest, NextApiResponse } from "next"
+import fetch from "node-fetch"
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.long}&appid=${process.env.WEATHER_API_KEY}`;
-    const data = await fetch(url);
-    const result = await data.json();
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${req.query.lat}&lon=${req.query.long}&appid=${process.env.WEATHER_API_KEY}`
+  const data = await fetch(url)
+  const result = await data.json()
 
-    const id = result?.weather?.[0]?.id;
-    const speed = result?.wind?.speed;
+  const id = result?.weather?.[0]?.id
+  const speed = result?.wind?.speed
 
-    res.status(200).json({ id, speed })
-  }
-  
+  res.status(200).json({ id, speed })
+}
